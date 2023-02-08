533585162f7b009cf0692cd591668de44d958bbf
CaptainCavalier <anthonyingegnere@googlemail.com>
2023-02-08 14:37:56 +0000

    added patch mapping

--- src/main/java/uk/sky/people/rest/PersonController.java
+++ src/main/java/uk/sky/people/rest/PersonController.java
@@ -1,11 +1,16 @@
 package uk.sky.people.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uk.sky.people.entities.Person;
+
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
+
+    private List<Person> people = new ArrayList<>();
 
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -16,6 +21,43 @@ public class PersonController {
     @RequestMapping(value = "home", method = RequestMethod.GET)
     public String welcomeHome() {
         return "THIS IS THE HOME PAGE!!!!";
+    }
+
+    @PostMapping("/create")
+    public Person addPerson(@RequestBody Person person) { // pulls person from the body of the request
+        this.people.add(person); // add a new person to the list
+        return this.people.get(this.people.size() - 1); // return the last person in the list
+    }
+
+    @GetMapping("/getAll")
+    public List<Person> getAll() {
+        return this.people;
+    }
+
+
+    @GetMapping("/get/{id}")
+    public Person getPerson(@PathVariable int id) {  //pulls id from the path (url)
+        System.out.println("id: " + id);
+        return this.people.get(id);
+    }
+
+    @PatchMapping("/update/{id}")
+    public Person updatePerson(@PathVariable int id, @PathParam("name") String name, @PathParam("age") Integer age, @PathParam("job") String job) {
+        Person old = this.people.get(id);
+
+        if (name != null) old.setName(name);
+        if (age != null) old.setAge(age);
+        if (job != null) old.setJob(job);
+
+
+        return old;
+
+    }
+
+
+    @DeleteMapping("/remove/{id}")
+    public Person removePerson(@PathVariable int id) {
+        return this.people.remove(id);
     }
 
 
